a70cdcd6993a48394f3eb6d4ffd847fff1a7593e
Aryanwankhade <147994373+Aryanwankhade@users.noreply.github.com>
2025-06-10 23:24:54 +0530

    Add files via upload

---
+++ Series Light Blinking.ino
@@ -0,0 +1,23 @@
+void setup() {
+  
+pinMode(26,OUTPUT);
+pinMode(27,OUTPUT);
+pinMode(25,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly
+  digitalWrite(26,HIGH);
+  delay(100);
+  digitalWrite(26,LOW);
+  delay(100);
+   digitalWrite(27,HIGH);
+  delay(100);
+  digitalWrite(27,LOW);
+  delay(100);
+   digitalWrite(25,HIGH);
+  delay(100);
+  digitalWrite(25,LOW);
+  delay(100 
+  );
+}
